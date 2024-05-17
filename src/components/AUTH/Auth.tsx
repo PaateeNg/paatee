9cdcdb328ac275060b2eb5502014565cb6aa01c1
Jamie Ohakwe <jamieohakwe@gmail.com>
2024-05-17 08:40:54 +0100

    create branch

--- src/components/AUTH/Auth.tsx
+++ src/components/AUTH/Auth.tsx
@@ -18,31 +18,34 @@ const Auth = () => {
         },
         {
             icon: "yh",
-            title: "ahh",
-            sub: "hey"
+            title: "Peace of mind guranteed",
+            sub: "Rest easy knowing that your event is in good hands"
         },
         {
             icon: "yh",
-            title: "ahh",
-            sub: "hey"
+            title: "Peace of mind guranteed",
+            sub: "Rest easy knowing that your event is in good hands"
         },
     ]
 	return (
 		<>
 			<div className="flex md:flex-row flex-col bg-gray-100 ">
                 {/* left */}
-				<div className="w-[746px]  rounded-xl overflow-hidden  bg-white m-[3rem] relative flex-1 " style={{ backgroundImage: `url('/assets/img/party-bg.jpg')` }}>
+				<div className="  rounded-xl overflow-hidden  bg-white m-[3rem] relative flex-1 " style={{ backgroundImage: `url('/assets/img/party-bg.jpg')` }}>
                         {/* image */}
-                        <Image height={100} width={200} alt='logo' src={logo} className='absolute top-[5rem]'/>
-                    <div className=" w-full bg-rose-300 absolute bottom-0 flex flex-col justify-center ">
+                        <Image height={100} width={200} alt='logo' src={logo} className='absolute top-[5rem] left-[3rem]'/>
+                    <div className=" w-full bg-rose-100 h-[70%] absolute bottom-0 flex flex-col justify-center  shadow rounded-xl  ">
                         {/* info */}
                     {signInfo.map((i, index)=>(
                         <>
-                            <div className="flex gap-2 mx-auto my-3 mb-6" key={index}>
-                                <div>
-                                    <Image height={50} width={50} alt='logo' src={icon}/>
-                                    <span>{i.title}</span><span>{i.sub}</span>
-                                </div>
+                            <div className="flex gap-2  mx-[3rem] " key={index}>
+                               
+                                    <Image height={50} width={50} alt='logo' src={icon} className=' absolute'/>
+                                    <div className='flex gap-2 flex-col justify-center mb-4 text-sm max-w-[27rem] ml-[4rem]'>
+
+                                        <span className='font-bold text-red-600  text-[1.2rem]'>{i.title}</span><span className='text-gray-600'>{i.sub}</span>
+                                    </div>
+                                
                                 
                             </div>
                         </>
@@ -50,13 +53,14 @@ const Auth = () => {
                     </div>
                 </div>
                 {/* right */}
-				<div className="h-[580px] w-[440px]  m-[2rem]  flex flex-col gap-4 justify-center">
+				<div className="h-[580px] max-w-[25rem] flex-1   m-[2rem]  flex flex-col gap-2 justify-center text-sm">
                         <span className='font-bold text-xl'>Sign in</span>
                         <span>Get into your account and begin exploring</span>
                         <div className='flex gap-2'>
-                            <div className='shadow py-3 text-sm px-10 rounded-full bg-white'>Sign in with Google</div>
-                            <div className='shadow py-3 px-5 rounded-full bg-white'>Sign in with Apple Id</div>
+                            <div className='shadow py-3 text-[13px] px-5 rounded-full bg-white'>Sign in with Google</div>
+                            <div className='shadow py-3 px-5 text-[13px] rounded-full bg-white'>Sign in with Apple Id</div>
                         </div>
+                        <hr className='mt-[2rem]' /> <span className='absolute '>or</span> 
                         <span>Email Address</span>
                         <input type="text" placeholder='jamieohakwe@gmail.com ' className='rounded-full'/>
                         <span>Password</span>
@@ -65,8 +69,8 @@ const Auth = () => {
                             <span>Keep me signed in</span>
                             <span className='text-red-600'>Forgot password</span>
                         </div>
-                        <button className='bg-red-500 rounded-full mt-4 h-[3rem] text-white'>Reset Password</button>
-                        <span>If you do not have an account with us, you could <span className='text-red-600'> <a href="">Sign Up</a></span></span>
+                        <button className='bg-red-500  rounded-full mt-4 h-[3rem] text-white'>Reset Password</button>
+                        <span className='text-[13px]'>If you do not have an account with us, you could <span className='text-red-600 text-center'> <a href="">Sign Up</a></span></span>
                 </div>
 			</div>
 		</>
