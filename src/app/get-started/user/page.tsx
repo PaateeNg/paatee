77e6f1f429aa25c29404f54410267818d0ca7c03
Jamie Ohakwe <jamieohakwe@gmail.com>
2024-06-09 17:27:12 +0100

    new logo

--- src/app/get-started/user/page.tsx
+++ src/app/get-started/user/page.tsx
@@ -1,101 +1,116 @@
 "use client";
 import React from "react";
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar/DesktopNavBar";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const VendorStart = () => {
 	return (
 		<>
-			<span className="flex item-center justify-center -mb-[3rem] text-[#F63D68] text-xl  mt-[2rem]">
-				Get started as a Vendor
-			</span>
-			<div className="flex m-[104px]  h-[448px] text-sm gap-[80px] ">
-				{/* Left */}
-				<div className="flex-2 flex flex-col w-[620px] h-[448px]">
-					<span>
-						Are you ready to turn your passion into profit? Become a
-						vendor and unlock a world of opportunities to showcase
-						your products and reach a wider audience. Whether
-						you&lsquo;re an artisan, a small business owner, or a
-						seasoned entrepreneur, our platform provides the perfect
-						launchpad for your success.
-					</span>
-					<span className="mt-[50px]">1. How It Works:</span>
-					<div className="ml-[20px] flex flex-col gap-[20px]">
+			<NavBar />
+			<div className="bg-gray-100">
+				<span className="flex item-center relative top-4 justify-center -mb-[3rem] text-[#F63D68]  text-xl font-bold mt-[2rem]">
+					Get started as a User
+				</span>
+				<div className="flex flex-row-reverse m-[104px]    text-md gap-[80px] ">
+					{/* Right */}
+					<div className="flex-2 flex flex-col max-w-[527px] h-[448px]">
 						<span>
-							a. Sign Up: Create your vendor account and complete
-							your profile with details about your products and
-							brand.
+							Ready to explore a world of endless possibilities?
+							Join our community as a customer and unlock access
+							to a wide range of products and services tailored to
+							your needs. Whether you&lsquo;re shopping for
+							essentials or seeking something special, we&lsquo;ve
+							got you covered.
 						</span>
-						<span>
-							b. List Your Products: Showcase your items with
-							high-quality photos and detailed descriptions to
-							entice buyers.
-						</span>
-						<span>
-							c. Manage Orders: Receive notifications of new
-							orders, process them efficiently, and ship your
-							products with ease.
-						</span>
-						<span>
-							d. Grow Your Business: Continuously refine your
-							offerings, engage with customers, and watch your
-							business flourish.
-						</span>
+						<span className="mt-[50px] text-xl text-gray-700 font-bold">How It Works:</span>
+						<div className="ml-[20px] flex flex-col gap-[20px]">
+							<span>
+								a. Sign Up: Create your customer account in
+								minutes and start exploring our vast
+								marketplace.
+							</span>
+							<span>
+								b. Browse and Shop: Browse through our extensive
+								catalog and add your favorite items to your
+								shopping cart with just a few clicks.
+							</span>
+							<span>
+								c. Checkout: Easily complete your purchase using
+								our secure payment gateway, and track your order
+								every step of the way.
+							</span>
+							<span>
+								d. Enjoy: Sit back, relax, and enjoy your new purchases as they&lsquo;re delivered right to your doorstep.
+							</span>
+						</div>
 					</div>
-				</div>
-				{/* Right */}
-				<div className="flex-1  flex flex-col w-[700px] h-[518px] text-xs ">
-					<span className="bg-[#FEF0C7] p-[3px] rounded-full text-center">
-						Please share some business information with us
-					</span>
-					<div className="flex flex-col gap-2">
-						<span className="mt-[20px] font-bold">
-							Name of business
+					{/* Left */}
+					<div className="flex-1  flex flex-col max-w-[536px]  bg-white rounded-xl p-[32px] text-sm shadow-xl">
+						<span className="bg-[#FEF0C7] p-[3px] h-[40px] text-lg  rounded-full text-center">
+							Please share some personal information with us
 						</span>
-						<input
-							type="text"
-							placeholder="jamieohakwe@gmail.com"
-							className="rounded-xl bg-white border border-gray-300"
-						/>
-						<span className="mt-[20px] font-bold">
-							Email address
-						</span>
-						<input
-							type="text"
-							placeholder="jamieohakwe@gmail.com"
-							className="rounded-xl bg-white border border-gray-300"
-						/>
-						<span className="mt-[20px] font-bold">
-							Business phone
-						</span>
-						<input
-							type="text"
-							placeholder="jamieohakwe@gmail.com"
-							className="rounded-xl bg-white border border-gray-300"
-						/>
+						<div className="flex flex-col">
+							<span className="mt-[20px] font-bold">
+								First name
+							</span>
+							<input
+								type="text"
+								placeholder="Enter first name"
+								className="rounded-xl bg-white border border-gray-300"
+							/>
+							<span className="mt-[20px] font-bold">
+								Last name
+							</span>
+							<input
+								type="text"
+								placeholder="Enter last name"
+								className="rounded-xl bg-white border border-gray-300"
+							/>
+							<span className="mt-[20px] font-bold">
+								Email address
+							</span>
+							<input
+								type="text"
+								placeholder="Enter email address"
+								className="rounded-xl bg-white border border-gray-300"
+							/>
+							<span className="mt-[20px] font-bold">
+								Phone number
+							</span>
+							<input
+								type="text"
+								placeholder="Enter phone"
+								className="rounded-xl bg-white border border-gray-300"
+							/>
+							<button className=" flex items-center justify-center text-white text-lg mt-[40px] w-[229px] py-3 bg-[#F63D68] rounded-full">
+								Next{" "}
+								<FaArrowRightLong className="relative left-3" />
+							</button>
+							<hr className="bg-[#F63D68] w-[40px] h-[6px] rounded-full relative left-[50%] bottom-7" />
+							<hr className="bg-gray-400 w-[10px] h-[6px] rounded-full relative left-[60%] bottom-8" />
+							<hr className="bg-gray-400 w-[10px] h-[6px] rounded-full relative left-[65%] bottom-9" />
+						</div>
+						<hr />
 					</div>
-					<button className=" text-white text-xl mt-[40px] w-[337px] h-[48px] bg-[#F63D68] rounded-full">
-						Next
-					</button>
-					<hr />
 				</div>
 			</div>
-			<div className="bg-[#FFF1F3] flex p-[32px] justify-between -mt-[7rem]">
+			<div className="bg-[#FFF1F3] flex p-[32px] justify-between ">
 				<div className="w-[620px] h-[116px] flex flex-col">
-					<span className="text-[#F63D68] ">
-						Looking to throw your next party?
+					<span className="text-[#F63D68] font-bold">
+					Want to sell stuff to people?
 					</span>
 					<span>
-						Whether it&lsquo;s a birthday bash, a corporate event,
-						or a special celebration, we&lsquo;ve got you covered.
-						From venue selection to entertainment, catering, and
-						everything in between, we&lsquo;ll bring your vision to
-						life. Let&lsquo;s make it unforgettable!{" "}
+					It&lsquo;s easy with us! Whether you&lsquo;re an established business or a budding entrepreneur, our platform provides the perfect space to showcase your products and reach eager customers. Join our community of sellers today and start turning your items into cash!
 					</span>
 				</div>
-				<div >
-					<span className="w-[233px] h-[48px] border border-[#F63D68] rounded-full  text-sm flex items-center justify-center ">Start as a customer  of</span>
+				<div>
+					<span className="w-[233px] h-[48px] border border-[#F63D68] rounded-full  text-sm flex items-center justify-center ">
+						Start as a vendor instead
+					</span>
 				</div>
 			</div>
+			<Footer />
 		</>
 	);
 };
