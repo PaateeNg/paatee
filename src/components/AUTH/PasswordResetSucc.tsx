93f89a101aa0a0e19b124e416e3c7c24e763e44c
Nwankwo Chidera <nwankwochidera.david@gmail.com>
2024-06-13 11:12:54 +0100

    should work

--- src/components/AUTH/PasswordResetSucc.tsx
+++ src/components/AUTH/PasswordResetSucc.tsx
@@ -6,11 +6,7 @@ const PasswordResetSucc = () => {
   return (
     <>
         <div>
-<<<<<<< HEAD
-            <div className='w-[392px]  border m-5 p-5 rounded-2xl border-red-700 bg-[#ffecec] flex flex-col gap-5 text-center'>
-=======
             <div className='w-[292px]  border m-5 p-5 rounded-2xl border-red-700 bg-[#ffecec] flex flex-col gap-5 text-center'>
->>>>>>> a25df2bb3ff37011ee441a5b7042703225abc75a
               <Image src={'/assets/img/tick.png'} height={50} width={50} alt='tick' className='m-auto flex'/>
                 <span className='font-bold text-xl'>Password Reset Successful </span>
                 <span className='text-gray-500'>You can now log into your account with your new password</span>
